2bc176a690ce22a04b28f85c1fa006f6cedc61d2
AbdElFateh <mahdios20181@gmail.com>
2020-12-08 23:44:57 +0100

    FATEH 08.12  a 12.00 MOdif

--- src/code/ControllerPurchase.java
+++ src/code/ControllerPurchase.java
@@ -2,9 +2,11 @@ package code;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class ControllerPurchase {
 
 
-
+    public void pieceSelected(MouseEvent mouseEvent) {
+    }
 }
